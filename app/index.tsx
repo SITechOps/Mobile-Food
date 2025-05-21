b90dabf11da0b7434725a5b871a41dfbde99cfbf
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2025-05-21 14:40:00 -0300

    feat: instalar React Native com Tailwind usando a ferramenta NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl">Olá</Text>
+    </View>
+  );
+}
