b0697cf35d7e2717830b5dc7bbc0f4a25259435f
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2025-06-05 17:31:10 -0300

    refactor: simplificar lógica para posicionar cursor no início do input multiline

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,7 @@
 import { Control, Controller } from "react-hook-form";
 import { View, Text, TextInput, TextInputProps } from "react-native";
 import { ProductFormData } from "../interfaces/IProduct";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 type InputProps = TextInputProps & {
   label: string;
@@ -10,7 +10,6 @@ type InputProps = TextInputProps & {
 };
 
 export function Input({ label, name, control, ...rest }: InputProps) {
-  const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef<TextInput>(null);
 
   return (
@@ -18,31 +17,21 @@ export function Input({ label, name, control, ...rest }: InputProps) {
       control={control}
       name={name}
       rules={{ required: true }}
-      render={({ field: { value, onChange, onBlur } }) => {
-        useEffect(() => {
-          // Só força a posição inicial se não estiver focado e for multiline
-          if (!isFocused && rest.multiline && value && inputRef.current) {
-            setTimeout(() => {
-              inputRef.current?.setSelection(0, 0);
-            }, 10);
-          }
-        }, [value, isFocused, rest.multiline]);
-
-        return (
-          <View className="flex-1 gap-2">
-            <Text className="font-heading text-xl">{label}:</Text>
-            <TextInput
-              ref={inputRef}
-              value={value || ""}
-              onChangeText={onChange}
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-              className="rounded-lg border border-transparent bg-[#f1f1f1] px-4 py-3 font-body text-lg focus:border-[#ee4c58]"
-              {...rest}
-            />
-          </View>
-        );
-      }}
+      render={({ field: { value, onChange } }) => (
+        <View className="flex-1 gap-2">
+          <Text className="font-heading text-xl">{label}:</Text>
+          <TextInput
+            ref={inputRef}
+            value={value}
+            onChangeText={onChange}
+            onLayout={() => {
+              rest.multiline && inputRef.current?.setSelection(0, 0);
+            }}
+            className="rounded-lg border border-transparent bg-[#f1f1f1] px-4 py-3 font-body text-lg focus:border-[#ee4c58]"
+            {...rest}
+          />
+        </View>
+      )}
     />
   );
 }
